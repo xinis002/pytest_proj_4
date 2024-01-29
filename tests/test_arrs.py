e77a6c60a68b56f58d0f4b062ffff6c742f9163a
Simon Inikhov <semikinihov@gmail.com>
2024-01-29 17:57:04 +0300

    add new tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -10,7 +10,7 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
 
-def test_get_index_out_of_bounds():
+def test_get_index_out_of_bouns():
     assert arrs.get([1, 2, 3], 5, "default") == "default"
 
 
